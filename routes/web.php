b75493dd7d6edbeca0ef86d1c3dca412a8fe9422
Anahit <--get-all>
2019-03-31 18:02:07 +0500

    photo Image.php file is  added

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/admin', 'HomeController@index')->name('home');
 Route::resource('/admin/categories', 'Admin\CategoriesController', ['as'=>'admin']);
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
 Route::resource('/admin/products', 'Admin\ProductsController', ['as'=>'admin']);
-=======
+//=======
 Route::resource('/admin/products', 'Admin\ProductsController', ['as'=>'admin']);
 Route::get('/admin/photo', 'PhotoController@index');
 Route::post('/admin/photo', 'PhotoController@upload');
->>>>>>> Stashed changes
+//>>>>>>> Stashed changes
